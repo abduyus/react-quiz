b2135d78fc6e13884868d03104bd6c522bd57238
abduyus <yusuf.abdurrasheed@outlook.com>
2025-08-02 14:56:35 +0500

    displaying questions and refactoring components into components folder

--- src/components/App.js
+++ src/components/App.js
@@ -11,6 +11,7 @@ const initialState = {
 
   //   'loading', 'error', 'ready', 'active', 'finished'
   status: "loading",
+  index: 0,
 };
 function reducer(state, action) {
   switch (action.type) {
@@ -20,13 +21,18 @@ function reducer(state, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1 };
     default:
       throw new Error("Action Unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
 
   const numQuestions = questions.length;
 
@@ -47,7 +53,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </Main>
     </div>
   );
